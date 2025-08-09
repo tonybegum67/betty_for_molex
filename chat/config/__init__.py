9647e5d688f441e43029e0a6a62707b7514930fa
Tony Begum <tonybegum@gmail.com>
2025-08-09 13:37:44 -0700

    Remove chat files from Betty repository - Betty is standalone

--- chat/config/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Configuration module for Chat RAG system"""
-from .settings import ChatConfig, RAGConfig, get_api_key, validate_config
-
-__all__ = ['ChatConfig', 'RAGConfig', 'get_api_key', 'validate_config']
--- chat/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Utilities for the Chat RAG system"""
-from .document_parser import DocumentParser, document_parser
-
-__all__ = ['DocumentParser', 'document_parser']
